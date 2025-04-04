3bd7ed54863183c5e93725d6ef37708f6d5269ff
Filip Hládek <71432962+Deam0on@users.noreply.github.com>
2025-04-04 18:32:28 +0200

    Update test_models.py
    Signed-off-by: Filip Hládek <71432962+Deam0on@users.noreply.github.com>

--- tests/test_models.py
+++ tests/test_models.py
@@ -31,20 +31,20 @@ def validate_fit(model_lambda, H0, true_params, initial_guess, bounds, rtol=1e-2
 
 def test_model_1_1():
     model = model_definitions(H0, G0, d_delta_exp)["1:1"]
-    assert validate_fit(model["lambda"], H0, PARAMS["1:1"], model["initial_guess"], model["bounds"])
+    assert validate_fit(model["lambda"], H0, PARAMS["1:1"], model["initial_guess"], model["bounds"], rtol=3e-2)
 
 def test_model_1_2():
     model = model_definitions(H0, G0, d_delta_exp)["1:2"]
-    assert validate_fit(model["lambda"], H0, PARAMS["1:2"], model["initial_guess"], model["bounds"])
+    assert validate_fit(model["lambda"], H0, PARAMS["1:2"], model["initial_guess"], model["bounds"], rtol=3e-2)
 
 def test_model_2_1():
     model = model_definitions(H0, G0, d_delta_exp)["2:1"]
-    assert validate_fit(model["lambda"], H0, PARAMS["2:1"], model["initial_guess"], model["bounds"])
+    assert validate_fit(model["lambda"], H0, PARAMS["2:1"], model["initial_guess"], model["bounds"], rtol=3e-2)
 
 def test_model_dimer():
     model = model_definitions(H0, G0, d_delta_exp)["dimer"]
-    assert validate_fit(model["lambda"], H0, PARAMS["dimer"], model["initial_guess"], model["bounds"])
+    assert validate_fit(model["lambda"], H0, PARAMS["dimer"], model["initial_guess"], model["bounds"], rtol=3e-2)
 
 def test_model_multi():
     model = model_definitions(H0, G0, d_delta_exp)["multi"]
-    assert validate_fit(model["lambda"], H0, PARAMS["multi"], model["initial_guess"], model["bounds"])
+    assert validate_fit(model["lambda"], H0, PARAMS["multi"], model["initial_guess"], model["bounds"], rtol=3e-2)
