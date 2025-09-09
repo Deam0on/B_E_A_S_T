651f825237fce45c7cbaa71a804cfdc568fa31ec
Filip Hládek <71432962+Deam0on@users.noreply.github.com>
2025-09-09 10:21:42 +0200

    Fix numerator calculation in chemical shift formula
    Signed-off-by: Filip Hládek <71432962+Deam0on@users.noreply.github.com>

--- binding_analysis/models.py
+++ binding_analysis/models.py
@@ -93,7 +93,7 @@ def binding_isotherm_1_2(
         )
 
         # Chemical shift calculation
-        numerator = d_inf_1 * G0_i * Ka * G_free + d_inf_2 * G0_i * Ka * Kd * G_free**2
+        numerator = d_inf_1 *  Ka * G_free + d_inf_2 * Ka * Kd * G_free**2
         denominator = 1 + Ka * G_free + Ka * Kd * G_free**2
 
         d_delta_comp[i] = numerator / denominator
